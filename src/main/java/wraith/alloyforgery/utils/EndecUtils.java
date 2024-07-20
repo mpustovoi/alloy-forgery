96b85cb1889d95c334e77676f9d41f6aa4f93a3e
Basique <basiqueevangelist@yandex.ru>
2024-07-21 00:17:47 +0300

    make CountedIngredient endec 🅱️ased

--- src/main/java/wraith/alloyforgery/utils/EndecUtils.java
+++ src/main/java/wraith/alloyforgery/utils/EndecUtils.java
@@ -1,11 +1,14 @@
 package wraith.alloyforgery.utils;
 
+import com.mojang.serialization.MapCodec;
 import io.wispforest.endec.Endec;
+import io.wispforest.endec.StructEndec;
 import io.wispforest.owo.serialization.CodecUtils;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.minecraft.recipe.Ingredient;
 
 public class EndecUtils {
+    public static StructEndec<Ingredient> STRUCT_INGREDIENT = new MapCodecStructEndec<>(MapCodec.assumeMapUnsafe(Ingredient.ALLOW_EMPTY_CODEC));
     public static Endec<Ingredient> INGREDIENT = CodecUtils.toEndec(Ingredient.ALLOW_EMPTY_CODEC);
     public static Endec<FluidVariant> FLUID_VARIANT = CodecUtils.toEndec(FluidVariant.CODEC).catchErrors((ctx, deserializer, e) -> FluidVariant.blank());
 }
